359d283a7502856dd93b2a177ae18afbbf9b9a6d
chenjie <657499186@qq.com>
2024-09-11 16:47:59 +0800

    err

---
+++ frame/msError/error.go
@@ -0,0 +1,17 @@
+package msError
+
+type Error struct {
+	Code int
+	Err  error
+}
+
+func (e *Error) Error() string {
+	return e.Err.Error()
+}
+
+func NewError(code int, err error) *Error {
+	return &Error{
+		Code: code,
+		Err:  err,
+	}
+}
